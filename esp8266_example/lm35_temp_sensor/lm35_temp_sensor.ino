5c6cd3c1633f7b2c691b81f55ebe7a05ea09d3b2
XiaoGuang Zeng <xgzeng@gmail.com>
2017-01-14 16:55:12 +0800

    analogRead max input is not 1V for nodecmu board

--- esp8266_example/lm35_temp_sensor/lm35_temp_sensor.ino
+++ esp8266_example/lm35_temp_sensor/lm35_temp_sensor.ino
@@ -2,28 +2,40 @@
 
 float history_temp = 0.0;
 
+// ADC_MODE(ADC_TOUT);
 void setup() {
   Serial.begin(115200);
-  
-  // Serial.setDebugOutput(true);
-  // Serial.print("Connected To ");
-  history_temp = read_temperature();
+
+  history_temp = analogReadToCelsius(analogRead(A0));
 }
 
-float read_temperature() {
-  // analogRead range: 0(0V, 0°C) - 1024(1V, 150°C)
-  int analog_voltage = analogRead(A0);
-  return float(analog_voltage * 150) /1024;
+/// nodecmu board has two resistor wired on ADC pin, so max input voltage is not 1V.
+///   3.422V is measured for my board, default should be 3.2V.
+const int MAX_ANALOG_INPUT_VOLTAGE_MV = 3422;
+
+float analogReadToVoltage(int value) {
+  return analogRead(A0) * MAX_ANALOG_INPUT_VOLTAGE_MV / 1023;  
+}
+
+float analogReadToCelsius(int value) {
+  return analogReadToVoltage(value)/10 + 2;
 }
 
 void loop() {
+  
+  int analog_read_value = analogRead(A0);
+  
+  float current_temp = analogReadToCelsius(analog_read_value);
+  
   // smoothing
-  float read_temp = read_temperature();
-  history_temp = history_temp * 0.8 + read_temp * 0.2;
+  history_temp = history_temp * 0.8 + current_temp * 0.2;
+  
   Serial.print("read value:");
-  Serial.print(read_temp);
+  Serial.print(analog_read_value);
+  Serial.print(" ");
+  Serial.print(current_temp);
   Serial.print(" smooth_value:");
   Serial.println((int)(history_temp * 10) / 10.0);
-  delay(1000);
+  delay(2000);
 }
 
